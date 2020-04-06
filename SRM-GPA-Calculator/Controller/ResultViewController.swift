b8a9f0ef585499053c9a40342c6e021fafe74230
Avaneesh <pathakavaneesh@gmail.com>
2020-04-06 16:58:37 +0530

    Add UI improvements

---
+++ SRM-GPA-Calculator/Controller/ResultViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ResultViewController.swift
+//  SRM-GPA-Calculator
+//
+//  Created by Avaneesh Pathak on 06/04/20.
+//  Copyright © 2020 Avaneesh Pathak. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UITableViewController {
+
+    @IBOutlet weak var resultLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    @IBAction func recalculateButtonPressed(_ sender: UIButton) {
+    }
+}
