2d3794e447a6b62675273ff5e03430351f4d6fda
Avaneesh <pathakavaneesh@gmail.com>
2020-04-07 13:38:42 +0530

    Add dismiss keyboard method

--- SRM-GPA-Calculator/Controller/CalculateViewController.swift
+++ SRM-GPA-Calculator/Controller/CalculateViewController.swift
@@ -12,7 +12,13 @@ class CalculateViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
 
     @IBAction func calculatePressed(_ sender: UIButton) {
